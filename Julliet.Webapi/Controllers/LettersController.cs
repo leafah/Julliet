c58dfa27b4e0b1084161c178d126079338758aa2
yuriperei <yuri.souza@al.infnet.edu,br>
2017-08-20 01:23:48 -0300

    Controllers

---
+++ Julliet.Webapi/Controllers/LettersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Julliet.Engine;
+using Julliet.Webapi.Repositories;
+using System.IO;
+using RestSharp;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Julliet.Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LettersController : Controller
+    {
+        private readonly LetterRepository _repository;
+
+        public LettersController()
+        {
+            this._repository = new LetterRepository();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]dynamic body)
+        {
+            JObject jObject = JObject.Parse(Convert.ToString(body));
+            var name = jObject.GetValue("name").Value<string>();
+            var letterType = jObject.GetValue("letterType").Value<string>();
+            var letter = jObject.GetValue("letter").Value<string>();
+
+            this._repository.WriteLetter(letter, Path.Combine(@"C:\Julliet", name), letterType + ".ltr");
+            
+            Response.WriteAsync($"The letter {name} was created com sucess");
+        }
+
+    }
+}
---
+++ Julliet.Webapi/Repositories/LetterRepository.cs
@@ -0,0 +1,31 @@
+﻿using Julliet.Engine;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Julliet.Webapi.Repositories
+{
+    public class LetterRepository
+    {
+
+        public void WriteLetter(string content, string path, string name)
+        {
+            if (!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+            }
+
+            using (var fileStream = new FileStream(Path.Combine(path, name), FileMode.OpenOrCreate))
+            using (var streamWriter = new StreamWriter(fileStream))
+            {
+                streamWriter.BaseStream.Position = 0;
+                streamWriter.WriteLine(content);
+            }
+
+        }
+    }
+}
